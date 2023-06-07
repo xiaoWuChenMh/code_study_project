7efe894a510be110758d1459d07ef6b0214592a4
v_hhuima_dr <v_hhuima@tencent.com>
2023-06-07 11:41:54 +0800

    springboot项目初始化

---
+++ springboot/src/main/java/com/future/springboot/Application.java
@@ -0,0 +1,18 @@
+package com.future.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author v_hhuima
+ * @Date 2023/6/5 11:42
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
